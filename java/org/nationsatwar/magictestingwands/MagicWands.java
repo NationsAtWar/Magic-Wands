4443d906854853fa04a4158fd07c75caa8a1dbd0
Feldherren <rpaliwoda@googlemail.com>
2014-09-30 22:51:02 +0100

    Incremented version. Also updated mcmod.info.

--- java/org/nationsatwar/magictestingwands/MagicWands.java
+++ java/org/nationsatwar/magictestingwands/MagicWands.java
@@ -17,7 +17,7 @@ public class MagicWands
 {
     public static final String MODID = "magictestingwands";
     public static final String NAME = "Magic Testing Wands";
-    public static final String VERSION = "1.7.10-0.1";
+    public static final String VERSION = "1.7.10-1.0";
     public static Configuration config;
     
     public static String wandCategory = "wands";
